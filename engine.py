f5e4800530f74f28296c633b50a89c2a38dff9dc
sevenlamps <yimingmichael@gmail.com>
2018-09-25 21:23:43 +0800

    3rd commit

---
+++ engine.py
@@ -0,0 +1,268 @@
+import logging
+import numpy as np
+from go import Board
+
+
+class Engine(object):
+    def __init__(self, move_stack, komi, time_settings):
+        self.board = Board()
+        self.move_stack = move_stack
+        self.komi = komi
+        self.time_settings = time_settings
+
+    # ############################### Administrative Commands #######################################
+    def protocol_version(self):
+        """
+        For gtp specification 2.
+        :return: int version number - Version of the GTP Protocol
+        """
+        return
+
+    def name(self):
+        """
+        E.g. “GNU Go”, “GoLois”, “Many Faces of Go”. The name does
+        not include any version information, which is
+        provided by the version command.
+        :return: string* name - Name of the engine
+        """
+        return
+
+    def version(self):
+        """
+        E.g. “3.1.33”, “10.5”. Engines without a sense of version
+        number should return the empty string.
+        :return: string* version - Version of the engine
+        """
+        return
+
+    def known_command(self, command_name):
+        """
+        The protocol makes no distinction between unknown commands
+        and known but unimplemented ones. Do not declare
+        a command as known if it is known not to work.
+        :param command_name: string command name - Name of a command
+        :return: boolean known - “true” if the command is known by
+        the engine, “false” otherwise
+        """
+        return
+
+    def list_commands(self):
+        """
+        Include all known commands, including required ones and
+        private extensions.
+        :return: string& commands - List of commands, one per row
+        """
+        return
+
+    def quit(self):
+        """
+        The full response of this command must be sent before the
+        engine closes the connection. The controller must receive
+        the response before the connection is closed on its side.
+        :return: None
+        """
+        return
+    # ############################### End of Administrative Commands #######################################
+
+    # ############################### Setup Commands #######################################################
+    def boardsize(self, size):
+        """
+        In GTP version 1 this command also did the work of
+        clear board. This may or may not be true for implementations
+        of GTP version 2. Thus the controller must
+        call clear board explicitly. Even if the new board size is
+        the same as the old one, the board configuration becomes
+        arbitrary.
+        :param size: int size - New size of the board.
+        :return: none
+        """
+        return
+
+    def clear_board(self):
+        """
+
+        :return: None
+        """
+        return
+
+    def komi(self, new_komi):
+        """
+        The engine must accept the komi even if it should be ridiculous.
+        :param new_komi: float new komi - New value of komi.
+        :return:
+        """
+        return
+
+    def fixed_handicap(self, number_of_stones):
+        """
+        This command is only valid if the board is empty. See
+        section 4.1.1 for valid number of handicap stones. The
+        handicap stones are not included in the move history.
+        :param number_of_stones: int number of stones - Number of handicap stones.
+        :return:vertex* vertices - A list of the vertices where handicap
+        stones have been placed.
+        """
+        return
+
+    def place_free_handicap(self, number_of_stones):
+        """
+        This command is only valid if the board is empty. The engine
+        may place fewer than the requested number of stones
+        on the board under certain circumstances, as discussed
+        in section 4.1.2. The controller can check this by counting
+        the number of vertices in the response. The handicap
+        stones are not included in the move history. Vertices must
+        not be repeated or include “pass”.
+        :param number_of_stones:
+        :return:vertex* vertices - A list of the vertices where handicap
+        stones have been placed.
+        """
+        return
+
+    def set_free_handicap(self, vertices):
+        """
+        This command is only valid if the board is empty. The
+        list must have at least two elements and no more than
+        the number of board vertices minus one. The engine must
+        accept the handicap placement. The handicap stones are
+        not included in the move history. Vertices must not be
+        repeated or include “pass”.
+        :param vertices: vertex* vertices - A list of vertices where handicap
+        stones should be placed on the board.
+        :return: None
+        """
+        return
+    # ############################### End of Setup Commands #######################################
+
+    # ############################### Core Play Commands #######################################################
+    def play(self, move):
+        """
+        Consecutive moves of the same color are not considered
+        illegal from the protocol point of view.
+        :param move:move move - Color and vertex of the move
+        :return:None
+        """
+        return
+
+    def genmove(self, color):
+        """
+        Notice that “pass” is a valid vertex and should be returned
+        if the engine wants to pass. Use “resign” if you want to
+        give up the game. The controller is allowed to use this
+        command for either color, regardless who played the last
+        move.
+        :param color:color color - Color for which to generate a move.
+        :return:vertex|string vertex - Vertex where the move was
+        played or the string “resign”.
+        """
+        return
+
+    def undo(self):
+        """
+        If you want to take back multiple moves, use this command
+        multiple times. The engine may fail to undo if the
+        move history is empty or if the engine only maintains a
+        partial move history, which has been exhausted by previous
+        undos. It is never possible to undo handicap placements.
+        Use clear board if you want to start over. An
+        engine which never is able to undo should not include this
+        command among its known commands.
+        :return:None
+        """
+        return
+    # ############################### End of Core Play Commands #######################################
+
+    # ############################### Tournament Commands #######################################################
+    def time_settings(self, main_time, byo_yomi_time, byo_yomi_stones):
+        """
+        The interpretation of the parameters is discussed in section
+        4.2. The engine must accept the requested values.
+        This command gives no provision for negotiation of the
+        time settings.
+        :param main_time:int main time - Main time measured in seconds.
+        :param byo_yomi_time:int byo yomi time - Byo yomi time measured in seconds.
+        :param byo_yomi_stones:int byo yomi stones - Number of stones per byo yomi period.
+        :return:None
+        """
+        return
+
+    def time_left(self, color, time, stones):
+        """
+        While the main time is counting, the number of remaining
+        stones is given as 0.
+        :param color:color color - Color for which the information applies.
+        :param time:int time - Number of seconds remaining.
+        :param stones:int stones - Number of stones remaining.
+        :return:None
+        """
+        return
+
+    def final_score(self):
+        """
+        ask for the engine’s opinion about the score
+        :return:string score - Score as described in section 4.3.
+        """
+        return
+
+    def final_status_list(self, status):
+        """
+        query an engine about the status of the stones
+        :param status:string status - Requested status.
+        :return:vertex*& stones - Stones with the requested status.
+
+        """
+        return
+    # ############################### End of Tournament Commands #######################################
+
+    # ############################### Regression Commands #######################################################
+    def loadsgf(self, filename, move_number):
+        """
+        Due to the syntactical limitations of this protocol, the
+        filename cannot include spaces, hash signs (#), or control
+        characters. The command requires the controller and the
+        engine to share file system, or at least that the controller
+        has sufficient knowledge about the file system of the engine.
+        If move number is larger than the number of moves
+        in the file, read until the end of the file. This command has
+        no support for sgf files with variations or game collections.
+        :param filename:string filename - Name of an sgf file.
+        :param move_number:int move number - Optional move number.
+        :return:None
+        """
+        return
+
+    def reg_genmove(self, color):
+        """
+        This command differs from genmove in that it does not
+        play the generated move. It is also advisable to turn off
+        any move randomization since that may cause meaningless
+        regression fluctuations.
+        :param color:color color - Color for which to generate a move.
+        :return:vertex|string vertex - Vertex where the engine would
+        want to play a move or the string “resign”.
+        """
+        return
+    # ############################### End of Regression Commands #######################################
+
+    # ############################### Debug Commands #######################################################
+    def showboard(self):
+        """
+        The engine may draw the board as it likes. It is, however,
+        required to place the coordinates as described in section
+        2.11. This command is only intended to help humans with
+        debugging and the output should never need to be parsed
+        by another program.
+        :return:string*& board - A diagram of the board position.
+        """
+        print('\n\n')
+        str_board = np.array([[self.board.p_symbol[val] for val in row] for row in self.board.board])
+        print('  '.join(['\tA', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']))
+        for index, line in enumerate(str_board):
+            if index < 9:
+                new_line = np.concatenate(([str(index+1)+' '], line, [str(index+1)]))
+            else:
+                new_line = np.concatenate(([str(index + 1)], line, [str(index + 1)]))
+            print('  '.join(new_line))
+
+        print('  '.join(['\tA', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']))
+        return
