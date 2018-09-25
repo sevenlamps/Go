4ffbdd4d978eb3143595922c8ee553c78cdec59b
sevenlamps <yimingmichael@gmail.com>
2018-09-25 16:21:30 +0800

    2nd commit

---
+++ go.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+BOARD_SIZE = (19, 19)
+
+
+class Board(object):
+    def __init__(self):
+        self.p_max = BOARD_SIZE[0] * BOARD_SIZE[1]  # 19 * 19 board
+        self.p_status = {'empty': 0b00, 'black': 0b01, 'white': 0b10, 'dummy': 0b11}
+        self.p_symbol = ['.', 'X', 'O', '.']
+        self.x_axis = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10,
+                       'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18
+                       }
+        self.y_axis = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '10': 9, '11': 10,
+                       '12': 11, '13': 12, '14': 13, '15': 14, '16': 15, '17': 16, '18': 17, '19': 18
+                       }
+
+        self.board = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+                              dtype=np.int8
+                              )
+
+    def print_board(self):
+        print('\n\n')
+        str_board = np.array([[self.p_symbol[val] for val in row] for row in self.board])
+        print('  '.join(['   A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']))
+        for index, line in enumerate(str_board):
+            new_line = np.concatenate(([str(index+1)], line, [str(index+1)]))
+            print('  '.join(new_line))
+
+        print('  '.join(['   A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']))
+
+    def get_vertex(self, coordinate):
+        # TODO what if a user wrongly keys in
+        x = self.x_axis[coordinate[0].upper()]
+        y = self.y_axis[coordinate[1]]
+        return self.board[x][y]
+
+    def set_vertex(self, coordinate, val):
+        # TODO what if a user wrongly keys in
+        x = self.x_axis[coordinate[0].upper()]
+        y = self.y_axis[coordinate[1]]
+        if self.board[x][y] == 0 and val != 0:
+            self.board[x][y] = val
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import go
+
+if __name__ == '__main__':
+    new_board = go.Board()
+    new_board.print_board()
